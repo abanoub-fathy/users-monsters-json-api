f2dc5918880a245de0f299ceea5a952150d6e3ba
pop-fathy <profpop14@gmail.com>
2021-11-08 19:28:13 +0200

    adding emails

--- index.js
+++ index.js
@@ -9,42 +9,52 @@ app.get("/monsters", (req, res) => {
     {
       name: "Abanoub",
       id: "215",
+      email: "aop4ever@gmail.com",
     },
     {
       name: "Mena",
       id: "sd5",
+      email: "aosfsa@gmail.com",
     },
     {
       name: "Teena",
       id: "asap",
+      email: "ateenr@gmail.com",
     },
     {
       name: "Mother",
       id: "sda7",
+      email: "anmamaer@gmail.com",
     },
     {
       name: "Father",
       id: "78aw",
+      email: "father@gmail.com",
     },
     {
       name: "Hathoot",
       id: "ytq",
+      email: "adsad5ver@gmail.com",
     },
     {
       name: "Afrooto",
       id: "wq8",
+      email: "sadiiw11@gmail.com",
     },
     {
       name: "koktail",
       id: "aw887w",
+      email: "sdsiauw45w@gmail.com",
     },
     {
       name: "Afrakoosh",
       id: "as88wi",
+      email: "sadsad555ar@gmail.com",
     },
     {
       name: "Zombie",
       id: "uytw8",
+      email: "wjopam44fa8@gmail.com",
     },
   ];
 
